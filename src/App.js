18180a49992de1a5c8b6c9e1bce9d4c12c61e9a7
jeky22 <qhzh33@gmail.com>
2021-07-16 17:08:41 +0900

    gnb 추가, css 조정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 // import logo from "./logo.svg";
 import "./App.css";
 import { contents } from "./data/contents";
-console.log(contents);
+import { MdSearch } from "react-icons/md";
+import { VscBell } from "react-icons/vsc";
+import { CgProfile } from "react-icons/cg";
 
 function List(list, isBold = 1) {
   return list.map((item, index) =>
@@ -20,12 +22,41 @@ function App() {
   const lastIndex = contents.length - 1;
 
   return (
-    <div className="container">
-      <div id="gnb">
+    <div>
+      <div className="container" id="gnb">
+        <div className="image-container">
+          <img src="/img/wanted-logo.png" alt="logo" />
+        </div>
+        <ul>
+          <li>탐색</li>
+          <li>커리어 성장</li>
+          <li>직군별 연봉</li>
+          <li>이력서</li>
+          <li>매치업</li>
+          <li>프리랜서</li>
+          <li>Ai 합격예측</li>
+        </ul>
+        <aside>
+          <div>
+            <MdSearch size={22} />
+          </div>
+          <div>
+            <VscBell size={22} />
+          </div>
+          <div className>
+            <CgProfile size={22} />
+          </div>
+          <div className="partition-vertical"></div>
+          <div className="btn">기업서비스</div>
+        </aside>
+      </div>
+      <div className="partition-horizontal"></div>
+      <div className="container" id="lnb">
         {data.map((item, index) => (
           <ul key={index}>{List(item, index === lastIndex ? 1 : 0)}</ul>
         ))}
       </div>
+      <div></div>
     </div>
   );
 }

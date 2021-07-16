86d0d4ac6f4a6a6085b948f5e068590e311daea2
jeky22 <qhzh33@gmail.com>
2021-07-16 10:08:20 +0900

    데이터, css적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 // import logo from "./logo.svg";
+import "./App.css";
+import { contents } from "./data/contents";
+console.log(contents);
+
+function List(list, isBold = 1) {
+  return list.map((item, index) =>
+    isBold === 1 || isBold === index ? (
+      <li className="bolder" key={index}>
+        {item}
+      </li>
+    ) : (
+      <li key={index}>{item}</li>
+    )
+  );
+}
 
 function App() {
+  const data = contents;
+  const lastIndex = contents.length - 1;
+
   return (
-    <div>
-      <ul>
-        <li>Hello</li>
-        <li>World</li>
-        <li>gg</li>
-        <li></li>
-        <li></li>
-      </ul>
+    <div className="container">
+      <div id="gnb">
+        {data.map((item, index) => (
+          <ul key={index}>{List(item, index === lastIndex ? 1 : 0)}</ul>
+        ))}
+      </div>
     </div>
   );
 }
